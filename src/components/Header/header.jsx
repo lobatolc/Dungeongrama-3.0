9417b7be57e59e8b9dbe6faf4e3d7f16bcb29ffd
Ajax Lima <ajaxlima94@gmail.com>
2022-02-27 19:23:37 -0300

    chore(header.jsx): change <a> for <Link> because we need unrefresh page

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Container } from './header.Styles';
 import logo from '../../images/icons/logo.png';
+import { logOutDungeongrama } from '../../services/firebaseUse';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  return(
+  function logOut() {
+    logOutDungeongrama();
+  }
+
+  return (
     <Container>
-        <div>
-            <img src={logo} alt=""/>
-            <h1>Dungeongrama</h1>
-        </div>
-        <div>
-            <a href="/stage">Jogar</a>
-            <a href="/ranking">Classificação</a>
-            <a href="/">Sair</a>
-        </div>
+      <div>
+        <img src={logo} alt="" />
+        <h1>Dungeongrama</h1>
+      </div>
+      <div>
+        <Link to="/stage">Jogar</Link>
+        <Link to="/ranking">Classificação</Link>
+        <Link onClick={logOut} to="/">
+          Sair
+        </Link>
+      </div>
     </Container>
-  )}
+  );
+}
 
 export default Header;
