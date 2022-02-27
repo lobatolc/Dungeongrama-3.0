6539cbdd6de55f465fd0a1d4f42bc142e5add4a4
Ajax Lima <ajaxlima94@gmail.com>
2022-02-27 19:20:11 -0300

    chore(notifycontext): format in the archive, for a better visual on read

--- src/contexts/notifyContext.jsx
+++ src/contexts/notifyContext.jsx
@@ -1,14 +1,22 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext } from 'react';
 
 const NotifyContext = createContext();
 
 export default function NotifyProvider(props) {
-    const [notifys, setNotifys] = useState({ log: "", type: 0, time: Date.now() });
+  const [notifys, setNotifys] = useState({
+    log: '',
+    type: 0,
+    time: Date.now(),
+  });
 
-    return <NotifyContext.Provider value={{ notifys, setNotifys }}>{props?.children}</NotifyContext.Provider>;
+  return (
+    <NotifyContext.Provider value={{ notifys, setNotifys }}>
+      {props?.children}
+    </NotifyContext.Provider>
+  );
 }
 
 export function useNotifys() {
-    const { notifys, setNotifys } = useContext(NotifyContext);
-    return { notifys, setNotifys };
+  const { notifys, setNotifys } = useContext(NotifyContext);
+  return { notifys, setNotifys };
 }
